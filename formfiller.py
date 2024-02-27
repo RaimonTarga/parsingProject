1f5500d6d9b2d571874f3498bfab6b7fd209403c
RaimonTarga <158033113+RaimonTarga@users.noreply.github.com>
2024-02-27 16:40:43 +0100

    Add files via upload

--- formfiller.py
+++ formfiller.py
@@ -6,22 +6,29 @@ from seleniumwire import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.service import Service
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) # Project Root
 INPUT_FILE_NAME = 'Listados.json'
 INPUT_PATH = os.path.join(ROOT_DIR, INPUT_FILE_NAME)
 URL = 'https://angular.specialisternespain.com/gestion-datos/#/tramitacion-pedidos'
-USERNAME = 'RaimonTarga-we60s4'
+USERNAME = ''
+OPTIONS = webdriver.ChromeOptions() 
+OPTIONS.add_experimental_option('excludeSwitches', ['enable-logging']) 
+SERVICE = Service('chromedriver.exe') 
+
 # si el valor es 0, no saltara ninguna ID y empezara por el principio
-SKIP_ID = 0
+try:
+    SKIP_ID = int(input('¿Quieres saltar a una ID en particular? Introduce 0 en el caso contrario\n'))
+except ValueError:
+    SKIP_ID = 0
 
 try:
     #intentamos abrir el archivo de listados
     with open(INPUT_PATH , 'r') as f:
         input_data = json.load(f)
-    
     #abrimos el driver
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(service=SERVICE, options=OPTIONS)
     driver.get(URL)
     #alta
     driver.find_element(By.XPATH, xpathstrings.BUTTON_ALTA_XPATH).click()
@@ -32,12 +39,11 @@ try:
 
     for item in input_data:
         #salta el elemento si hemos especificado una ID a la que queremos salta
-        if (SKIP_ID != 0 & int(item['ID_Cliente']) <= SKIP_ID):
+        if (SKIP_ID != 0 and int(item['ID_Cliente']) < SKIP_ID):
             continue
-        #introduce ID
+        #introduce ID cliente
         driver.find_element(By.XPATH, xpathstrings.ID_CLIENTE_XPATH).send_keys(item['ID_Cliente'])
         driver.find_element(By.XPATH, xpathstrings.ID_CLIENTE_BUTTON_XPATH).click()
-        
         try:
             #hemos encontrado el mensaje de error
             overlay = driver.find_element(By.CLASS_NAME, xpathstrings.OVERLAY_CLASS)
