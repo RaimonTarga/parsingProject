e3bc2897abce9ee4702ce1020027a9b24b708c43
RaimonTarga <158033113+RaimonTarga@users.noreply.github.com>
2024-02-27 14:27:27 +0100

    Add files via upload

--- formfiller.py
+++ formfiller.py
@@ -1,6 +1,96 @@
 import json
+import os
+import xpathstrings
 from time import sleep
 from seleniumwire import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 
-driver = webdriver.Chrome(executable_path='chromedriver.exe')
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) # Project Root
+INPUT_FILE_NAME = 'Listados.json'
+INPUT_PATH = os.path.join(ROOT_DIR, INPUT_FILE_NAME)
+URL = 'https://angular.specialisternespain.com/gestion-datos/#/tramitacion-pedidos'
+USERNAME = 'RaimonTarga-we60s4'
+# si el valor es 0, no saltara ninguna ID y empezara por el principio
+SKIP_ID = 0
+
+try:
+    #intentamos abrir el archivo de listados
+    with open(INPUT_PATH , 'r') as f:
+        input_data = json.load(f)
+    
+    #abrimos el driver
+    driver = webdriver.Chrome()
+    driver.get(URL)
+    #alta
+    driver.find_element(By.XPATH, xpathstrings.BUTTON_ALTA_XPATH).click()
+    #introducir contraseña
+    driver.find_element(By.XPATH, xpathstrings.PASS_XPATH).send_keys(USERNAME)
+    driver.find_element(By.XPATH, xpathstrings.PASS_BUTTON_XPATH).click()
+    sleep(2)
+
+    for item in input_data:
+        #salta el elemento si hemos especificado una ID a la que queremos salta
+        if (SKIP_ID != 0 & int(item['ID_Cliente']) <= SKIP_ID):
+            continue
+        #introduce ID
+        driver.find_element(By.XPATH, xpathstrings.ID_CLIENTE_XPATH).send_keys(item['ID_Cliente'])
+        driver.find_element(By.XPATH, xpathstrings.ID_CLIENTE_BUTTON_XPATH).click()
+        
+        try:
+            #hemos encontrado el mensaje de error
+            overlay = driver.find_element(By.CLASS_NAME, xpathstrings.OVERLAY_CLASS)
+            #cierra el mensaje de error
+            driver.find_element(By.XPATH, xpathstrings.ERRORBUTTON_XPATH).click()
+            #borra el campo de ID
+            driver.find_element(By.XPATH, xpathstrings.ID_CLIENTE_XPATH).click()
+            driver.find_element(By.XPATH, xpathstrings.ID_CLIENTE_XPATH).send_keys(Keys.CONTROL + "a")
+            driver.find_element(By.XPATH, xpathstrings.ID_CLIENTE_XPATH).send_keys(Keys.DELETE)
+            sleep(1)
+        except NoSuchElementException:
+            #no hemos encontrado el mensaje de error y por lo tanto, la ID es valida
+            #introduce ID de facturacion
+            driver.find_element(By.XPATH, xpathstrings.ID_FACTURACION_XPATH).send_keys(item['ID_Facturacion'])
+            driver.find_element(By.XPATH, xpathstrings.ID_FACTURACION_BUTTON_XPATH).click()
+            #guarda los valores que necesitaremos para llenar los campos
+            pin1 = driver.find_element(By.XPATH, xpathstrings.PIN1_XPATH).get_attribute('value')
+            clave1 = driver.find_element(By.XPATH, xpathstrings.CLAVE1_XPATH).get_attribute('value')
+            pin2 = driver.find_element(By.XPATH, xpathstrings.PIN2_XPATH).get_attribute('value')
+            clave2 = driver.find_element(By.XPATH, xpathstrings.CLAVE2_XPATH).get_attribute('value')
+            pedido = driver.find_element(By.XPATH, xpathstrings.PEDIDO_XPATH).get_attribute('value')
+            cantidad = driver.find_element(By.XPATH, xpathstrings.CANTIDAD_XPATH).get_attribute('value')
+            #introduce valores de control 1
+            driver.find_element(By.XPATH, xpathstrings.CONTROL_PIN1_XPATH).send_keys(pin1)
+            driver.find_element(By.XPATH, xpathstrings.CONTROL_CLAVE1_XPATH).send_keys(clave1)
+            driver.find_element(By.XPATH, xpathstrings.CONTROL_BUTTON1_XPATH).click()
+            #guarda numero de control 1
+            control1 = driver.find_element(By.XPATH, xpathstrings.DIGITO_CONTROL1_XPATH).get_attribute('value')
+            #introduce valores de control 2
+            driver.find_element(By.XPATH, xpathstrings.CONTROL_PIN2_XPATH).send_keys(pin2)
+            driver.find_element(By.XPATH, xpathstrings.CONTROL_CLAVE2_XPATH).send_keys(clave2)
+            driver.find_element(By.XPATH, xpathstrings.CONTROL_BUTTON2_XPATH).click()
+            #guarda numero de control 2
+            control2 = driver.find_element(By.XPATH, xpathstrings.DIGITO_CONTROL2_XPATH).get_attribute('value')
+            #introduce numeros de control, n. pedido y cantidad
+            driver.find_element(By.XPATH, xpathstrings.TRANSACCION_CONTROL1_XPATH).send_keys(control1)
+            driver.find_element(By.XPATH, xpathstrings.TRANSACCION_CONTROL2_XPATH).send_keys(control2)
+            driver.find_element(By.XPATH, xpathstrings.TRANSACCION_PEDIDO_XPATH).send_keys(pedido)
+            driver.find_element(By.XPATH, xpathstrings.TRANSACCION_CANTIDAD_XPATH).send_keys(cantidad)    
+            driver.find_element(By.XPATH, xpathstrings.SIGUIENTE_BUTTON_XPATH).click()
+            sleep(2)
+            #introduce numero de articulos y codigo internacional
+            driver.find_element(By.XPATH, xpathstrings.NUMARTICULOS_XPATH).send_keys(item['N_Articulos'])
+            driver.find_element(By.XPATH, xpathstrings.CODIGO_XPATH).send_keys(item['Cod_Internacional'])
+            #si necesitamos generar factura, activa el checkbox
+            if bool(item['Generar_factura']):
+                driver.find_element(By.XPATH, xpathstrings.CHECKBOX_XPATH).click()
+            driver.find_element(By.XPATH, xpathstrings.GENERAR_BUTTON_XPATH).click()
+            sleep(2)
+    #cierra el driver
+    driver.quit()
+except IOError:
+    print('Error: el archivo ' + INPUT_FILE_NAME + ' no existe o no se puede encontrar')
+
+    
+    
---
+++ xpathstrings.py
@@ -0,0 +1,40 @@
+#alta
+BUTTON_ALTA_XPATH = '/html/body/div[1]/div/section/article[1]/button'
+#contraseña
+PASS_XPATH = '/html/body/div[2]/div/div[2]/input'
+PASS_BUTTON_XPATH = '/html/body/div[2]/div/div[3]/div[2]/button'
+#ID cliente y facturacion
+ID_CLIENTE_XPATH = '/html/body/div[1]/div/form/div/div[1]/input'
+ID_CLIENTE_BUTTON_XPATH = '/html/body/div[1]/div/form/div/div[1]/button'
+ID_FACTURACION_XPATH = '/html/body/div[1]/div/form/div/div[2]/input'
+ID_FACTURACION_BUTTON_XPATH = '/html/body/div[1]/div/form/div/div[2]/button'
+#valores control 1
+PIN1_XPATH = '/html/body/div[1]/div/form/div/div[3]/div[2]/input[1]'
+CLAVE1_XPATH = '/html/body/div[1]/div/form/div/div[3]/div[2]/input[2]'
+CONTROL_PIN1_XPATH = '/html/body/div[1]/div/form/div/div[5]/div[1]/input[1]'
+CONTROL_CLAVE1_XPATH = '/html/body/div[1]/div/form/div/div[5]/div[1]/input[2]'
+CONTROL_BUTTON1_XPATH = '/html/body/div[1]/div/form/div/div[5]/div[1]/button'
+DIGITO_CONTROL1_XPATH = '/html/body/div[1]/div/form/div/div[5]/div[2]/input'
+#valores control 2
+PIN2_XPATH = '/html/body/div[1]/div/form/div/div[4]/div[2]/input[1]'
+CLAVE2_XPATH = '/html/body/div[1]/div/form/div/div[4]/div[2]/input[2]'
+CONTROL_PIN2_XPATH = '/html/body/div[1]/div/form/div/div[6]/div[1]/input[1]'
+CONTROL_CLAVE2_XPATH = '/html/body/div[1]/div/form/div/div[6]/div[1]/input[2]'
+CONTROL_BUTTON2_XPATH = '/html/body/div[1]/div/form/div/div[6]/div[1]/button'
+DIGITO_CONTROL2_XPATH = '/html/body/div[1]/div/form/div/div[6]/div[2]/input'
+#campos de valores de control 1 y 2
+PEDIDO_XPATH = '/html/body/div[1]/div/form/div/div[4]/div[1]/input[2]'
+CANTIDAD_XPATH = '/html/body/div[1]/div/form/div/div[4]/div[1]/input[3]'
+TRANSACCION_CONTROL1_XPATH = '/html/body/div[1]/div/form/div/div[7]/div[1]/input'
+TRANSACCION_CONTROL2_XPATH = '/html/body/div[1]/div/form/div/div[7]/div[2]/input'
+TRANSACCION_PEDIDO_XPATH = '/html/body/div[1]/div/form/div/div[7]/div[3]/input'
+TRANSACCION_CANTIDAD_XPATH = '/html/body/div[1]/div/form/div/div[7]/div[4]/input'
+SIGUIENTE_BUTTON_XPATH = '/html/body/div[1]/div/form/div/button[1]'
+#numero de articulos y codigo internacional
+NUMARTICULOS_XPATH = '/html/body/div[1]/div/form/div/div/div[5]/input'
+CODIGO_XPATH = '/html/body/div[1]/div/form/div/div/div[6]/input'
+CHECKBOX_XPATH = '/html/body/div[1]/div/form/div/div/input'
+GENERAR_BUTTON_XPATH = '/html/body/div[1]/div/form/div/button[2]'
+#mensaje de error
+OVERLAY_CLASS = 'swal-overlay--show-modal'
+ERRORBUTTON_XPATH = '/html/body/div[2]/div/div[3]/div/button'
